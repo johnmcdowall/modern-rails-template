3a47b3d218f76080064a985077ec21b52358c820
John McDowall <john@mcdowall.info>
2017-11-01 13:00:07 -0700

    Update template location

--- template.rb
+++ template.rb
@@ -75,7 +75,7 @@ def add_template_repository_to_source_path
     at_exit { FileUtils.remove_entry(tempdir) }
     git :clone => [
       "--quiet",
-      "https://github.com/mattbrictson/rails-template.git",
+      "https://github.com/johnmcdowall/modern-rails-template",
       tempdir
     ].map(&:shellescape).join(" ")
 
